c78b71a7fa471669d975e09737da6094f28560e3
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-15 10:18:51 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+
+<script>
+export default {
+  name: "Navbar",
+  props: {},
+  data() {
+    return {
+        title: 'Wiki Countries'
+    }
+  },
+  computed: {},
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">{{ title }}</a>
+    </div>
+  </nav>
+</template>
+
+<style >
+</style>
